a86b40250959328c4cccf22088fb0a0bf1710faa
Deepak Rajendran <deepak.rajendran123@gmail.com>
2024-01-24 02:46:51 -0500

    Added header and footer templates

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,9 +1,13 @@
 import "./App.css";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
 
 function App() {
   return (
     <>
+      <Header />
       <div>Hello World</div>
+      <Footer />
     </>
   );
 }
