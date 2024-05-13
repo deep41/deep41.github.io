e4aad7bf3176e850b07061f9c79a2fca7de8816f
Deepak Rajendran <deepak.rajendran123@gmail.com>
2024-05-13 16:24:58 -0400

    work component

--- src/components/portfolio/work.tsx
+++ src/components/portfolio/work.tsx
@@ -1,10 +1,36 @@
 type WorkType = {};
 
+const workList = [
+  {
+    name: "Work 1",
+    from: "May 2024",
+    to: "Dec 2023",
+    points: ["Hello world", "Point 2"],
+  },
+];
+
 const Work = (props: WorkType) => {
   return (
     <>
-      <div className="px-6 py-4 bg-red-100">
-        <div>Work info</div>
+      <div className="px-6 py-2">
+        <div className="font-bold text-lg">Work Experience</div>
+        {workList.map((item) => (
+          <div>
+            <div className="flex flex-row justify-between">
+              <div>{item.name}</div>
+              <div>
+                {item.from} - {item.to}
+              </div>
+            </div>
+            <div>
+              <ul>
+                {item.points.map((point) => (
+                  <li> {point}</li>
+                ))}
+              </ul>
+            </div>
+          </div>
+        ))}
       </div>
     </>
   );
