5409fe1d4148c481dbd8429723917f116383fb1f
Deepak Rajendran <deepak.rajendran123@gmail.com>
2024-05-16 13:58:44 -0400

    updated education component to include both institutes

--- src/components/portfolio/about.tsx
+++ src/components/portfolio/about.tsx
@@ -1,32 +1,31 @@
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 
+const aboutData = {
+  title:
+    "I'm Deepak Rajendran, a software developer with a passion forbuilding innovative solutions.",
+  points: [
+    "Transforming ideas into innovative solutions, I'm a driven software developer with a passion for building cutting-edge applications.",
+    "With a strong foundation in computer science and 1.7 years of experience, I excel in web development, backend engineering, and cloud technologies.",
+    "Let's connect and bring your vision to life!",
+  ],
+};
+
 type AboutType = {};
 
 const About = (props: AboutType) => {
   return (
     <>
       <div className="px-6 py-2">
-        <div className="md:flex md:flex-direction-row flex-direction-column items-start mb-8">
+        <div className="md:flex md:flex-direction-row flex-direction-column items-center mb-8">
           <Avatar className="h-40 w-40 md:h-60 md:w-60 m-4 md:mb-0 md:text-center mx-auto">
             <AvatarImage src="https://avatars.githubusercontent.com/u/22258487?v=4" />
-            <AvatarFallback>CN</AvatarFallback>
+            <AvatarFallback>DR</AvatarFallback>
           </Avatar>
-          <div className="md:ml-4 ml-0">
-            <p className="text-lg font-bold">
-              I'm Deepak Rajendran, a software developer with a passion for
-              building innovative solutions.
-            </p>
-            <p className="text-gray-600">
-              Transforming ideas into innovative solutions, I'm a driven
-              software developer with a passion for building cutting-edge
-              applications. With a strong foundation in computer science and 1.7
-              years of experience, I excel in web development, backend
-              engineering, and cloud technologies. Let's connect and bring your
-              vision to life!
-            </p>
-            <p className="text-gray-600">
-              When I'm not coding, you can find me hiking or playing guitar.
-            </p>
+          <div className="md:ml-12 ml-0">
+            <p className="text-lg font-bold">{aboutData.title}</p>
+            {aboutData.points.map((point) => (
+              <p className="text-gray-600 mt-2 ">{point}</p>
+            ))}
           </div>
         </div>
       </div>
--- src/components/portfolio/education.tsx
+++ src/components/portfolio/education.tsx
@@ -3,12 +3,31 @@ type EducationType = {};
 const educationList = [
   {
     name: "North Carolina State Univeristy",
+    degree: "Masters in Computer Science",
     from: "Aug 2023",
     to: "May 2025",
-    points: ["GPA: 3.84", "Hack NC Winner", "Point C"],
+    points: [
+      "GPA: 3.84",
+      "Hack NC 2024 Winner",
+      "Tech Crew member at University Theatre",
+    ],
     image:
       "https://upload.wikimedia.org/wikipedia/commons/thumb/e/e1/North_Carolina_State_University_Athletic_logo.svg/1200px-North_Carolina_State_University_Athletic_logo.svg.png",
     imagealt: "NCSU logo",
+  },
+  {
+    name: "Ramaiah Institute of Technology",
+    degree: "Bachelors of Engineering in Computer Science and Engineering",
+    from: "July 2015",
+    to: "June 2019",
+    points: [
+      "CGPA: 8.66",
+      "Hackathons won: init 2017",
+      "Department Fest Organiser",
+    ],
+    image:
+      "https://upload.wikimedia.org/wikipedia/en/5/5a/Ramaiah_Institutions_Logo.png",
+    imagealt: "RIT logo",
   },
 ];
 
@@ -16,35 +35,30 @@ const Education = (props: EducationType) => {
   return (
     <>
       <div className="px-6 py-2">
-        <div className="font-bold text-lg">Education</div>
+        <div className="font-bold text-2xl">Education</div>
         {educationList.map((item) => (
           <div className="px-6 py-2">
             <div className="md:flex md:flex-direction-row flex-direction-column items-start mb-8">
               <img
                 src={item.image}
-                alt="item.imagealt"
-                className="max-h-32 max-w-32 p-2 m-2 mx-auto"
+                alt={item.imagealt}
+                className="max-h-32 max-w-32 p-2 m-2 mx-auto md:mx-0"
               />
-              <div className="md:ml-4 ml-0">
-                <p className="">
-                  <div className="flex flex-col items-baseline md:flex-row  md:justify-between">
-                    <div className="pr-2 text-lg font-bold">{item.name}</div>
-                    <div className="text-md italic text-balance">
-                      {item.from} - {item.to}
-                    </div>
+              <div className="md:ml-12 ml-0 w-full">
+                <div className="flex flex-col items-baseline md:flex-row  md:justify-between">
+                  <div className="pr-2 text-lg font-bold">{item.name}</div>
+                  <div className="text-md italic text-balance">
+                    {item.from} - {item.to}
                   </div>
-                </p>
-                <p className="text-gray-600">
-                  Transforming ideas into innovative solutions, I'm a driven
-                  software developer with a passion for building cutting-edge
-                  applications. With a strong foundation in computer science and
-                  1.7 years of experience, I excel in web development, backend
-                  engineering, and cloud technologies. Let's connect and bring
-                  your vision to life!
-                </p>
-                <p className="text-gray-600">
-                  When I'm not coding, you can find me hiking or playing guitar.
-                </p>
+                </div>
+                <div className="text-sm italic">{item.degree}</div>
+                {item.points &&
+                  item.points.map((point) => (
+                    <p className="text-gray-600 items-baseline">
+                      <span className="text-2xl">•</span>
+                      {point}
+                    </p>
+                  ))}
               </div>
             </div>
           </div>
