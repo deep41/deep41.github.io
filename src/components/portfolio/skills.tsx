d52c4e5e8458f3c23b0d36a39ce5fd7ea745b045
Deepak Rajendran <deepak.rajendran123@gmail.com>
2024-06-13 09:53:20 -0400

    fix regression in light mode

--- src/components/portfolio/skills.tsx
+++ src/components/portfolio/skills.tsx
@@ -7,13 +7,13 @@ const Skills = (props: SkillsType) => {
   return (
     <>
       <div className="bg-gray-100 px-6 py-4 dark:bg-black">
-        {/* <div className="font-bold text-2xl">Technical skills</div> */}
+        <div className="mb-2 text-2xl font-bold">Skills</div>
         <div className="flex flex-row flex-wrap gap-2">
           {skills &&
             skills.map((item, index) => (
               <div
                 key={`${item}-${index}`}
-                className="cursor-default bg-blue-950 p-2 font-mono text-sm text-white transition hover:scale-110 dark:hover:scale-95"
+                className="cursor-default bg-blue-950 p-2 font-mono text-sm text-white transition hover:scale-110 hover:bg-blue-950 dark:bg-blue-950/50 dark:hover:scale-95"
               >
                 {item}
               </div>
