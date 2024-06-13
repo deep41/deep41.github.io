8e41c8fa7368de7a65aec8ff750d337ba67c123c
Deepak Rajendran <deepak.rajendran123@gmail.com>
2024-06-13 09:42:08 -0400

    fix build failing

--- src/components/portfolio/header.tsx
+++ src/components/portfolio/header.tsx
@@ -16,17 +16,7 @@ const Header = (props: HeaderType) => {
     }
   }, []);
 
-  const toggleTheme = () => {
-    console.log("toggleTheme");
-    const htmlElement = document.documentElement;
-    if (htmlElement.classList.contains("dark")) {
-      htmlElement.classList.remove("dark");
-      localStorage.setItem("theme", "light");
-    } else {
-      htmlElement.classList.add("dark");
-      localStorage.setItem("theme", "dark");
-    }
-  };
+
 
   return (
     <>
