db0b6d7e77fff6ecb0fbfa0a63490651479bf2de
Deepak <deepak.rajendran123@gmail.com>
2025-02-19 22:37:13 -0500

    Add project images and update project display styling

--- src/components/projects/projectsData.ts
+++ src/components/projects/projectsData.ts
@@ -7,7 +7,7 @@ const projects: ProjectItem[] = [
       'Built an AI agent to manage venue and vendor coordination, logistics, scheduling, contract negotiations, marketing, and post-event follow-ups, while providing a streamlined dashboard.',
     tags: ['React', 'Node.js', 'TypeScript', 'MongoDB'],
     github: 'https://github.com/deep41/EventPlannerAI',
-    image: 'https://placehold.co/600x400',
+    image: '/projects/eventflow/eventflow.png',
     slug: 'eventplanner-ai',
     // longDescription: 'This is the longer description!'
   },
@@ -17,7 +17,7 @@ const projects: ProjectItem[] = [
       'Orchestrated services using Docker and Ansible for seamless deployment and management within Virtual Private Clouds (VPCs) and container clusters across multiple devices.',
     tags: ['Python', 'Ansible', 'Linux', 'Networking'],
     github: 'https://github.com/deep41/mixxcloud',
-    image: 'https://placehold.co/600x400',
+    image: '/projects/mixxcloud/mixxcloud.jpeg',
     slug: 'load-balanced-service-orchestrator',
   },
   {
@@ -26,7 +26,7 @@ const projects: ProjectItem[] = [
       'Built a spaced repetition tool that recommends LeetCode problems, helping 35+ users practice efficiently and improve problem-solving skills.',
     tags: ['React', 'Node.js', 'TypeScript', 'MongoDB'],
     live: 'https://leetrevise.com',
-    image: 'https://placehold.co/600x400',
+    image: '/projects/leetrevise/leetrevise.png',
     slug: 'leetrevise',
   },
 ];
